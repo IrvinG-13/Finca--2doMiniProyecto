d0bc4f95565e7bdd2d036aae6eedd220e9401a98
irvin gonzalez <irvigonza13@gmail.com>
2024-11-01 19:03:52 -0500

    primer commit

---
+++ FincaPrimavera.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+class Producto{
+    //atributos
+    private String nombre;
+    private String trimestres[];
+}
+
+class Trimestre{
+    //atributos
+    private double kilosP;
+    private double kilosV;
+    private double precioPromedio;
+    private double costoDeProduccion;
+}
+
+//Algoritmo Principal
+public class FincaPrimavera{
+    public static void main(String[] args){
+
+    }
+}
